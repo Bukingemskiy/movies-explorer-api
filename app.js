e83c82f18878a155d3cc12284abe0c6374f82ef0
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-09-14 22:55:05 +0300

    remove helmet and limiter

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const { errors } = require('celebrate');
 const cors = require('./middlewares/cors');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorsHandler = require('./middlewares/errorsHandler');
-const limiter = require('./middlewares/rateLimiter');
+// const limiter = require('./middlewares/rateLimiter');
 
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 mongoose.connect(NODE_ENV === 'production'
@@ -22,12 +22,12 @@ mongoose.connect(NODE_ENV === 'production'
 
 const app = express();
 app.use(requestLogger);
-app.use(limiter);
+// app.use(limiter);
 app.use(cors);
 app.use(express.json());
 app.use(router);
 app.use(cookieParser());
-app.use(helmet());
+// app.use(helmet());
 app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
