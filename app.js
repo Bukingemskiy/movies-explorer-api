26d2949b2d14015d7d1141f70fb2f20219087e66
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-10 20:32:11 +0300

    fixed cors

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
