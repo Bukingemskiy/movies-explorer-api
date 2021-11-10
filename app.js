16b72bfd27f498563720fa75323279ed16e0bc0a
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-10 20:26:40 +0300

    cors

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
