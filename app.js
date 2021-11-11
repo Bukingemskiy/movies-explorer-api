0cefc9be8f1289dec6beb6b3b15f7700f05ea4ac
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-11 19:44:59 +0300

    origin:true

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(
   cors({
-    origin: "https://prof.movies.explorer.nomoredomains.club/",
+    origin: true,
     credentials: true,
   })
 );
