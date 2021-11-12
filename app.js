45a10a1f379ddbdfd836be640622d16963230992
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-12 20:43:18 +0300

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
-const cors = require("cors");
+const cors = require("./middlewares/cors");
 const router = require("./routes/index");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const errorsHandler = require("./middlewares/errorsHandler");
@@ -25,17 +25,12 @@ mongoose.connect(
 );
 
 const app = express();
+app.use(cors);
 app.use(requestLogger);
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(
-  cors({
-    origin: "https://prof.movies.explorer.nomoredomains.club",
-    credentials: true,
-  })
-);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
---
+++ middlewares/cors.js
@@ -0,0 +1,28 @@
+/* eslint-disable consistent-return */
+/* eslint-disable quotes */
+
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+const allowedCors = [
+  "http://prof.movies.explorer.nomoredomains.club/",
+  "https://prof.movies.explorer.nomoredomains.club",
+  "localhost:3000",
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers["access-control-request-headers"];
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    return res.end();
+  }
+  next();
+};
+
+module.exports = cors;
