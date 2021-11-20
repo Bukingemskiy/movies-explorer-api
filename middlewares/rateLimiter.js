a64857d929a997a8e71debf34b57236bbad0080a
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-11-20 20:33:42 +0300

    fix limiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,8 +3,8 @@
 const rateLimiter = require("express-rate-limit");
 
 const limiter = rateLimiter({
-  windowMs: 10 * 60 * 1000,
-  max: 10000,
+  windowMs: 60 * 1000,
+  max: 1000,
 });
 
 module.exports = limiter;
